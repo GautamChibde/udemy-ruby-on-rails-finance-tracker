051d0905449864cc04239fe98d249ebd90642731
gautamchibde <gautamchibde@gmail.com>
2018-03-14 23:07:03 +0530

    user and stock assosiation

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
         if params[:stock].blank?
             flash.now[:danger] = "Empty search"
         else
-            @stock = Stock.new_form_lookup(params[:stock])
+            @stock = Stock.new_from_lookup(params[:stock])
             flash.now[:danger] = "Stock not found" unless @stock
         end
         render partial: 'users/results'
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ActiveRecord::Base
     has_many :user_stocks
     has_many :users, through: :user_stocks
-    def self.new_form_lookup(ticker_symbol)
+    def self.new_from_lookup(ticker_symbol)
         begin
             looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
             new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: strip_commas(looked_up_stock.l)) 
@@ -12,5 +12,9 @@ class Stock < ActiveRecord::Base
 
     def self.strip_commas(number)
         number.gsub(",","")
+    end
+
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
     end
 end
